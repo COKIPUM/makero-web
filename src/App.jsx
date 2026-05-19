8b8210b2c63048e7433f24280cdb79959f912c10
COKIPUM <cokipum@gmail.com>
2026-05-19 18:15:08 +0200

    React Router para páginas legales

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar       from './components/Navbar'
 import Hero         from './components/Hero'
 import Servicios    from './components/Servicios'
@@ -13,44 +14,41 @@ import AvisoLegal   from './components/AvisoLegal'
 import Privacidad   from './components/Privacidad'
 import Cookies      from './components/Cookies'
 
-function getCurrentPage() {
-  const path = window.location.pathname
-  if (path === '/aviso-legal') return 'aviso-legal'
-  if (path === '/privacidad') return 'privacidad'
-  if (path === '/cookies') return 'cookies'
-  return 'home'
+function Home() {
+  return (
+    <main>
+      <Hero />
+      <div className="divider" />
+      <Servicios />
+      <div className="divider" />
+      <Galeria />
+      <div className="divider" />
+      <Promociones />
+      <div className="divider" />
+      <ComoFunciona />
+      <div className="divider" />
+      <Testimonials />
+      <div className="divider" />
+      <About />
+      <div className="divider" />
+      <Redes />
+      <div className="divider" />
+      <Contacto />
+    </main>
+  )
 }
 
 export default function App() {
-  const page = getCurrentPage()
-
-  if (page === 'aviso-legal') return <><Navbar /><AvisoLegal /><Footer /></>
-  if (page === 'privacidad')  return <><Navbar /><Privacidad /><Footer /></>
-  if (page === 'cookies')     return <><Navbar /><Cookies /><Footer /></>
-
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <main>
-        <Hero />
-        <div className="divider" />
-        <Servicios />
-        <div className="divider" />
-        <Galeria />
-        <div className="divider" />
-        <Promociones />
-        <div className="divider" />
-        <ComoFunciona />
-        <div className="divider" />
-        <Testimonials />
-        <div className="divider" />
-        <About />
-        <div className="divider" />
-        <Redes />
-        <div className="divider" />
-        <Contacto />
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aviso-legal" element={<AvisoLegal />} />
+        <Route path="/privacidad" element={<Privacidad />} />
+        <Route path="/cookies" element={<Cookies />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
